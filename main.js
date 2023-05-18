45a505885a1b91619e4d68413dac25c12acb3a4f
IdR3n <bidrisarikaya@gmail.com>
2023-05-18 22:11:00 +0200

    J'ai remplacé l'axios.get() JavaScript->HTML

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
-// méthode axios/await
-async function getFetes(){
-    try {
-        const res = await axios.get('https://date.nager.at/api/v3/publicholidays/2023/CH');
-        console.log(res);
-        const fetesList = res.data
-        const holElement = document.getElementById('calendar');
 
-        for (let fetes of fetesList) {
-            const fetesEl = document.createElement('div')
-            fetes.innerHTML=
-            
-      fetesEl.classList.add('col')
-      fetesEl.addEventListener('click', function(){
-        alert(fetes.date)
-      })
-      holElement.appendChild(fetesEl)
-        }
-    } catch (e) {
-        throw e;
-    }
-}
-getFetes()
+
+//getFetes()
 
 
